fed0cf25d860418e124c92a05ca93eed5d7f2c86
Alexander Muratov <alexandermuratov2000@ya.ru>
2023-04-13 15:53:43 +0300

    FL-18: Color Scheme Added

---
+++ FlingoLingo/Modules/Sources/UIComponents/ColorScheme.swift
@@ -0,0 +1,18 @@
+//
+//  ColorScheme.swift
+//  FlingoLingo
+//
+//  Created by Alexander Muratov on 4/12/23.
+//
+
+import UIKit
+
+public enum ColorScheme {
+    
+    public static let fill = UIColor(red: 210/255, green: 67/255, blue: 102/255, alpha: 1.0)
+    public static let label = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+    public static let secondaryLabel = UIColor(red: 175/255, green: 175/255, blue: 175/255, alpha: 1.0)
+    public static let background = UIColor(red: 27/255, green: 26/255, blue: 26/255, alpha: 1.0)
+    public static let secondaryBackground = UIColor(red: 18/255, green: 18/255, blue: 18/255, alpha: 1.0)
+    public static let inactive = UIColor(red: 99/255, green: 99/255, blue: 99/255, alpha: 1.0)
+}
