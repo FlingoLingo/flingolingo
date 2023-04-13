38d6bafe7fbc88df478d993f8e1a567b61b46324
Alexander Muratov <alexandermuratov2000@ya.ru>
2023-04-13 15:54:00 +0300

    FL-18: Fonts Added

---
+++ FlingoLingo/Modules/Sources/UIComponents/Fonts.swift
@@ -0,0 +1,17 @@
+//
+//  Fonts.swift
+//  FlingoLingo
+//
+//  Created by Alexander Muratov on 4/12/23.
+//
+
+import UIKit
+
+public enum Fonts {
+    
+    public static let largeTitle = UIFont.systemFont(ofSize: 32, weight: .bold)
+    public static let title = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    public static let body = UIFont.systemFont(ofSize: 17, weight: .regular)
+    public static let footnote = UIFont.systemFont(ofSize: 15, weight: .regular)
+    public static let caption = UIFont.systemFont(ofSize: 13, weight: .regular)
+}
