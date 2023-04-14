ee90699f74b8bfc5990b884b67c0bc7182b765d9
Alexander Muratov <alexandermuratov2000@ya.ru>
2023-04-14 19:23:54 +0300

    FL-7: Create authorization flow

---
+++ FlingoLingo/Modules/Sources/Authorization/AuthorizationFlow/AuthorizationView.swift
@@ -0,0 +1,205 @@
+//
+//  AuthorizationView.swift
+//  FlingoLingo
+//
+//  Created by Alexander Muratov on 4/12/23.
+//
+
+import UIKit
+import UIComponents
+
+protocol AuthorizationViewDelegate: AnyObject {
+    func backButtonTapped()
+    func continueButtonTapped(mail: String, password: String)
+}
+
+class AuthorizationView: UIView {
+    
+    // MARK: - Properties
+    private lazy var navigationBarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorScheme.background
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private lazy var navigationBarBackButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+        button.tintColor = ColorScheme.mainText
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var navigationBarTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedString("authorizationViewNavigationBarTitleLabel", comment: "")
+        label.font = Fonts.largeTitle
+        label.numberOfLines = 1
+        label.textColor = ColorScheme.mainText
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedString("authorizationViewDescriptionLabel", comment: "")
+        label.numberOfLines = 0
+        label.font = Fonts.mainText
+        label.textColor = ColorScheme.secondaryText
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var mailTextField: InformationTextField = {
+        let placeholderText = NSLocalizedString("mailTextFieldPlaceholder", comment: "")
+        let textField = InformationTextField(placeholderText: placeholderText)
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private lazy var passwordTextField: InformationTextField = {
+        let placeholderText = NSLocalizedString("passwordTextFieldPlaceholder", comment: "")
+        let textField = InformationTextField(placeholderText: placeholderText)
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private lazy var continueButton: MainButton = {
+        let title = NSLocalizedString("continueButton", comment: "")
+        let button = MainButton(title: title, titleColor: ColorScheme.mainText, backgroundColor: ColorScheme.accent)
+        button.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    weak var delegate: AuthorizationViewDelegate?
+    private var didSetupConstraints = false
+    
+    // MARK: - Initialization
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        addSubviews()
+        setupConstraints()
+        addViewGestureRecognizer()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Module functions
+    private func setupViews() {
+        backgroundColor = ColorScheme.background
+    }
+    
+    private func setupConstraints() {
+        setupNavigationBarConstraints()
+        setupDescriptionLabelConstraints()
+        setupTextFieldsConstraints()
+        setupContinueButtonConstraints()
+    }
+    
+    private func addSubviews() {
+        addSubview(navigationBarView)
+        navigationBarView.addSubview(navigationBarBackButton)
+        navigationBarView.addSubview(navigationBarTitleLabel)
+        addSubview(descriptionLabel)
+        addSubview(mailTextField)
+        addSubview(passwordTextField)
+        addSubview(continueButton)
+    }
+    
+    private func addViewGestureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        addGestureRecognizer(tap)
+    }
+    
+    // MARK: - Actions
+    @objc private func backButtonTapped() {
+        delegate?.backButtonTapped()
+    }
+    
+    @objc private func continueButtonTapped() {
+        guard let mail = mailTextField.text, let password = passwordTextField.text else { return }
+        delegate?.continueButtonTapped(mail: mail, password: password)
+    }
+    
+    @objc func dismissKeyboard() {
+        endEditing(true)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        if text.isEmpty {
+            textField.layer.borderColor = ColorScheme.inactive.cgColor
+        } else {
+            textField.layer.borderColor = ColorScheme.mainText.cgColor
+        }
+    }
+}
+
+// MARK: - Setup constraints
+extension AuthorizationView {
+    
+    private func setupNavigationBarConstraints() {
+        NSLayoutConstraint.activate([
+            navigationBarView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            navigationBarView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            navigationBarView.topAnchor.constraint(equalTo: topAnchor),
+            navigationBarView.heightAnchor.constraint(equalToConstant: CommonConstants.safeAreaInsetsTop + 44 + 40),
+            
+            navigationBarBackButton.leadingAnchor.constraint(equalTo: navigationBarView.leadingAnchor, constant: CommonConstants.bigSpacing),
+            navigationBarBackButton.heightAnchor.constraint(equalToConstant: CommonConstants.navigationBarIconSide),
+            navigationBarBackButton.widthAnchor.constraint(equalToConstant: CommonConstants.navigationBarIconSide),
+            navigationBarBackButton.centerYAnchor.constraint(equalTo: navigationBarTitleLabel.centerYAnchor),
+            
+            navigationBarTitleLabel.leadingAnchor.constraint(equalTo: navigationBarBackButton.trailingAnchor, constant: CommonConstants.smallSpacing),
+            navigationBarTitleLabel.bottomAnchor.constraint(equalTo: navigationBarView.bottomAnchor),
+        ])
+    }
+    
+    private func setupDescriptionLabelConstraints() {
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: navigationBarView.bottomAnchor, constant: CommonConstants.smallSpacing),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: CommonConstants.bigSpacing),
+            trailingAnchor.constraint(equalTo: descriptionLabel.trailingAnchor, constant: CommonConstants.bigSpacing)
+        ])
+    }
+    
+    private func setupTextFieldsConstraints() {
+        NSLayoutConstraint.activate([
+            mailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: CommonConstants.bigSpacing),
+            trailingAnchor.constraint(equalTo: mailTextField.trailingAnchor, constant: CommonConstants.bigSpacing),
+            mailTextField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: CommonConstants.bigSpacing),
+            mailTextField.heightAnchor.constraint(equalToConstant: CommonConstants.textFieldHeight),
+            
+            passwordTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: CommonConstants.bigSpacing),
+            trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor, constant: CommonConstants.bigSpacing),
+            passwordTextField.topAnchor.constraint(equalTo: mailTextField.bottomAnchor, constant: CommonConstants.smallSpacing),
+            passwordTextField.heightAnchor.constraint(equalToConstant: CommonConstants.textFieldHeight),
+        ])
+    }
+    
+    private func setupContinueButtonConstraints() {
+        NSLayoutConstraint.activate([
+            continueButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: CommonConstants.bigSpacing),
+            trailingAnchor.constraint(equalTo: continueButton.trailingAnchor, constant: CommonConstants.bigSpacing),
+            safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: continueButton.bottomAnchor, constant: CommonConstants.bigSpacing),
+            continueButton.heightAnchor.constraint(equalToConstant: CommonConstants.buttonHeight)
+        ])
+    }
+}
---
+++ FlingoLingo/Modules/Sources/Authorization/AuthorizationFlow/AuthorizationViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AuthorizationViewController.swift
+//  FlingoLingo
+//
+//  Created by Alexander Muratov on 4/12/23.
+//
+
+import UIKit
+
+public class AuthorizationViewController: UIViewController {
+    
+    // MARK: - Properties
+    private lazy var authorizationView: AuthorizationView = {
+        let view = AuthorizationView()
+        view.delegate = self
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func loadView() {
+        view = authorizationView
+    }
+}
+
+// MARK: - AuthorizationViewDelegate
+extension AuthorizationViewController: AuthorizationViewDelegate {
+    
+    func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func continueButtonTapped(mail: String, password: String) {
+        print("mail: \(mail), password: \(password)")
+    }
+}
