feb88455b67191669ba22111076031dbbd49067c
Alexander Muratov <alexandermuratov2000@ya.ru>
2023-04-16 21:11:18 +0300

    FL-8: fix bug with navbar

--- FlingoLingo/SceneDelegate.swift
+++ FlingoLingo/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let navigationConroller = UINavigationController(rootViewController: WelcomeViewController())
+        navigationConroller.navigationBar.isHidden = true
         window.rootViewController = navigationConroller
         self.window = window
         window.makeKeyAndVisible()
