a3392ed129fc87ea937b5eaa9b83a17ed29f900b
Yana Barbashina <barbashina015@mail.ru>
2023-04-17 18:20:45 +0300

    FL-26: localizable added

--- FlingoLingo/Modules/Sources/UserProfile/ViewModels/UserViewModel.swift
+++ FlingoLingo/Modules/Sources/UserProfile/ViewModels/UserViewModel.swift
@@ -1,13 +1,22 @@
 import Foundation
 
 public final class UserViewModel: ObservableObject {
-
+    
     enum TextField: Equatable {
         case old
         case new
         case confirm
     }
-
+    
+    enum PasswordError: String {
+        case notEnoughSymbols = "notEnoughSymbols"
+        case notEqual = "notEqual"
+        
+        func localizedString() -> String {
+            return NSLocalizedString(self.rawValue, comment: "")
+        }
+    }
+    
     @Published var user: User
     @Published var oldPassword: String = ""
     @Published var newPassword: String = ""
@@ -16,10 +25,10 @@ public final class UserViewModel: ObservableObject {
     @Published var newPasswordCorrect: Bool = true
     @Published var confirmPasswordCorrect: Bool = true
     @Published var validatePasswords: [TextField: String] = [:]
-
+    
     private let router: UserProfileRouter
     private let backAction: () -> Void
-
+    
     init(user: User = User(),
          backAction: @escaping () -> Void = {},
          router: UserProfileRouter = UserProfileRouter()) {
@@ -27,62 +36,62 @@ public final class UserViewModel: ObservableObject {
         self.backAction = backAction
         self.router = router
     }
-
+    
     func changePassword() {
-
+        
         if oldPassword.count < 6 {
-            validatePasswords[.old] = "Пароль должен состоять как минимум  из 6 символов"
+            validatePasswords[.old] = PasswordError.notEnoughSymbols.localizedString()
             oldPasswordCorrect = false
         } else {
             validatePasswords[.old] = nil
             oldPasswordCorrect = true
         }
-
+        
         if newPassword.count < 6 {
-            validatePasswords[.new] = "Пароль должен состоять как минимум  из 6 символов"
+            validatePasswords[.new] = PasswordError.notEnoughSymbols.localizedString()
             newPasswordCorrect = false
         } else {
             validatePasswords[.new] = nil
             newPasswordCorrect = true
         }
-
+        
         if confirmPassword != newPassword {
-            validatePasswords[.confirm] = "Пароли не совпадают"
+            validatePasswords[.confirm] = PasswordError.notEqual.localizedString()
             confirmPasswordCorrect = false
         } else {
             validatePasswords[.confirm] = nil
             confirmPasswordCorrect = true
         }
-
+        
         if oldPasswordCorrect && newPasswordCorrect && confirmPasswordCorrect {
             // change password
         }
     }
-
+    
     func getErrorText(for type: TextField) -> String? {
         validatePasswords[type]
     }
-
+    
     func hasError(for type: TextField) -> Bool {
         validatePasswords[type] != nil
     }
-
+    
     func logOut() {
-
+        
     }
-
+    
     func openWelcomeView() {
-
+        
     }
-
+    
     func openSettings() {
         router.changePassword(user: user)
     }
-
+    
     func goBack() {
         backAction()
     }
-
+    
     func settingsIconClicked() {
         router.changePassword(user: self.user)
     }
