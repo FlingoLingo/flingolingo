eab4961573eff433420644ec012f3a6348c4ccb7
kidjuniper <kidjuniper@yandex.ru>
2023-04-18 16:35:33 +0300

    change sd

--- FlingoLingo/SceneDelegate.swift
+++ FlingoLingo/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let navigationConroller = UINavigationController(rootViewController: DictionaryViewController())
+        let navigationConroller = UINavigationController(rootViewController: CustomTabBarController())
         navigationConroller.navigationBar.isHidden = true
         window.rootViewController = navigationConroller
         self.window = window
