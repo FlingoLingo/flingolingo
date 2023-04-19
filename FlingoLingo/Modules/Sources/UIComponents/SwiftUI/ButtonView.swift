dd83984e27371bedebb8b946c810146e40c3bbc5
alisa <alisavy2010@gmail.com>
2023-04-19 10:01:28 +0300

    FL-61: removed effects

--- FlingoLingo/Modules/Sources/UIComponents/SwiftUI/ButtonView.swift
+++ FlingoLingo/Modules/Sources/UIComponents/SwiftUI/ButtonView.swift
@@ -9,13 +9,6 @@ import SwiftUI
 
 public struct ButtonView: View {
 
-    private enum Constants {
-        static let blurRadius: CGFloat = 4
-        static let shadowRadius: CGFloat = 15
-        static let offset: CGFloat = -10
-        static let padding: CGFloat = 8
-    }
-
     private let buttonText: String
     private let buttonClicked: (() -> Void)
     private let animationDuration = 0.1
@@ -32,32 +25,13 @@ public struct ButtonView: View {
             .foregroundColor(SColors.mainText)
             .frame(maxWidth: .infinity)
             .padding(.all, CommonConstants.smallSpacing)
-            .background {
-                ZStack {
-                    SColors.accent
-                    RoundedRectangle(cornerRadius: CommonConstants.cornerRadius, style: .continuous)
-                        .foregroundColor(SColors.lightButton)
-                        .blur(radius: Constants.blurRadius)
-                        .offset(x: Constants.offset, y: Constants.offset)
-                    RoundedRectangle(cornerRadius: CommonConstants.cornerRadius, style: .continuous)
-                        .fill(LinearGradient(gradient: Gradient(colors: [SColors.accent, SColors.lightButton]),
-                                           startPoint: .topLeading,
-                                           endPoint: .bottomTrailing))
-                        .padding(Constants.padding)
-                        .blur(radius: Constants.blurRadius)
-                }
-                .blur(radius: Constants.blurRadius)
-            }
+            .background(SColors.accent)
             .cornerRadius(CommonConstants.cornerRadius)
-            .shadow(color: SColors.darkButton,
-                    radius: Constants.shadowRadius,
-                    x: Constants.padding / 2,
-                    y: Constants.padding / 2)
             .onTapGesture {
-                buttonClicked()
                 animate = true
                 DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration, execute: {
                     self.animate = false
+                    buttonClicked()
                 })
             }
             .scaleEffect(animate ? 0.9 : 1)
