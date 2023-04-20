7c1ba5e2aeaec420e3f67addea95ac1acb13d25c
playatlas <atlasafri@gmail.com>
2023-04-20 19:27:14 +0300

    added a card status update method to net layer and backend

--- FlingoLingo/Modules/Sources/NetworkLayer/DeckClient.swift
+++ FlingoLingo/Modules/Sources/NetworkLayer/DeckClient.swift
@@ -32,6 +32,11 @@ public struct DeckNameRequest: Encodable {
     var name: String
 }
 
+public struct CardKnowledgeRequest: Encodable {
+    public var cardsLearned: [Int]
+    public var cardsForgotten: [Int]
+}
+
 public enum DecksError: Error {
     case jsonEncodeError
     case jsonDecodeError
@@ -86,6 +91,18 @@ public final class DeckClient {
                                                     name: name),
                                   completion: completion)
     }
+
+    public func updateCardKnowledge(id: Int,
+                                    cardsLearned: [Int],
+                                    cardsForgotten: [Int],
+                                    completion: @escaping (Result<MessageResponse, ClientError>) -> Void) {
+        self.netLayer.makeRequest(method: "PUT",
+                                  urlPattern: "/decks/\(id)/",
+                                  body: CardKnowledgeRequest(cardsLearned: cardsLearned,
+                                                             cardsForgotten: cardsForgotten),
+                                  completion: completion)
+    }
+
     public func deleteDeck(id: Int, completion: @escaping (Result<MessageResponse, ClientError>) -> Void) {
         self.netLayer.makeRequest(method: "DELETE",
                                   urlPattern: "/decks/\(id)/",
